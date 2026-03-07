a6b3e87d6a573e7b1f54259013ba1480ba762320
meiboxiao <meiboxiao@smail.nju.edu.cn>
2026-03-07 17:12:18 +0800

    feat: implement run method of safe_busprocess

---
+++ execution/safe_subprocess/__init__.py
@@ -0,0 +1,21 @@
+import os
+import signal
+import subprocess
+
+
+def run(cmd, timeout=10):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, start_new_session=True)
+
+    try:
+        stdout, stderr = p.communicate(timeout=timeout)
+
+    except subprocess.TimeoutExpired:
+        os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+
+        return {"timeout": True}
+
+    return {"timeout": False,
+            "exitcode": p.returncode,
+            "stdout": stdout,
+            "stderr": stderr
+            }
