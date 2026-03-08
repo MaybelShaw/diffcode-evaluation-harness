d70d34090f35b0ddfac56be9e3e95299244237ff
meiboxiao <meiboxiao@smail.nju.edu.cn>
2026-03-08 18:14:43 +0800

    feat: implement PassK

--- metrics/passk.py
+++ metrics/passk.py
@@ -1,9 +1,16 @@
+import math
 from base import Metric
 
 
 class PassK(Metric):
-    def __init__(self):
-        pass
+    def __init__(self, k):
+        self.k = k
 
-    def evaluate(self, **kwargs):
-        pass
+    def evaluate(self, results):
+        n = len(results)
+        c = sum(results)
+
+        if n - c < self.k:
+            return 1.0
+
+        return 1 - math.comb(n - c, self.k) / math.comb(n, self.k)
